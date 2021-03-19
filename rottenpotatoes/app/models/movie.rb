fa148a93f25f321a4401d60a6602039d2d8f1036
EC2 Default User <ec2-user@ip-172-31-39-231.us-east-2.compute.internal>
2021-03-19 03:18:29 +0000

    hw4_add_hw3_features

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,8 +1,12 @@
 class Movie < ActiveRecord::Base
+  def self.find_similar_movies(director)
+    Movie.where(director: director)
+  end
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
-  def self.find_similar_movies(director)
-    Movie.where(director: director)
+    
+  def self.with_ratings(ratings)
+      where("LOWER(rating) IN (?)", ratings.map(&:downcase))
   end
 end
